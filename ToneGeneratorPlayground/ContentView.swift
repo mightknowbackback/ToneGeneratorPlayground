556c8ef25e89a8d87b369f4dd2536cb3a5b41ed1
mightknow <michaeljohnbackes@gmail.com>
2020-11-06 18:06:25 -0600

    Initial Commit

---
+++ ToneGeneratorPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToneGeneratorPlayground
+//
+//  Created by mightknow on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
