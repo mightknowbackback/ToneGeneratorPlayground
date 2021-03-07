0072d82f519f784670264d41bfc0fdfc126f9043
mbackes <mbackes@uno.edu>
2021-03-07 15:33:57 -0600

    Add notes to ViewModel

--- ToneGeneratorPlayground/ViewModel/ViewModel.swift
+++ ToneGeneratorPlayground/ViewModel/ViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import SwiftUI
 
 class ViewModel : ObservableObject {
+    // Strings for Waveform Picker
     var waveformStrings : [String] {
         var result : [String] = []
         for w in Waveform.allCases {
@@ -16,19 +17,21 @@ class ViewModel : ObservableObject {
         }
         return result
     }
+    // Binding for setting waveform from picker
     var waveformBinding : Binding<Int> {
         Binding<Int>(
             get: {
                 return Waveform.allCases.firstIndex(of: self.audioModel.synth.waveform)!
-                
             },
             set: {i in
                 self.audioModel.synth.waveform = Waveform.allCases[i]
             }
         )
     }
+    // Readable string for waveform frequency, rounded to one decimal.
     var frequencyLabelString : String {
         return String(format: "Frequency:\n%.1f", self.audioModel.synth.frequency)
     }
+    // Model
     @Published var audioModel = AudioModel()
 }
