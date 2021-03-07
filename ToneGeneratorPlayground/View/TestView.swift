9e10ef0de7a78810728c3655631130bca950733d
mbackes <mbackes@uno.edu>
2021-03-07 15:25:47 -0600

    Add notes to TestView and test git push from Terminal

--- ToneGeneratorPlayground/View/TestView.swift
+++ ToneGeneratorPlayground/View/TestView.swift
@@ -11,18 +11,23 @@ struct TestView: View {
     @EnvironmentObject var viewModel : ViewModel
     var body: some View {
         VStack {
+            // Waveform Picker Label
             Text("Waveform:")
                 .multilineTextAlignment(.center)
                 .padding()
+            // Waveform Picker
             Picker("", selection: self.viewModel.waveformBinding) {
                 ForEach(0..<self.viewModel.waveformStrings.count) {i in
                     Text(self.viewModel.waveformStrings[i])
                 }
             }
+            // Frequency Label
             Text(self.viewModel.frequencyLabelString)
                 .multilineTextAlignment(.center)
                 .padding()
+            // Frequency Slider
             Slider(value: self.$viewModel.audioModel.synth.frequency, in: 220.0...880.0).padding()
+            // Start/Stop Buttons
             HStack {
                 Button(action: {
                     self.viewModel.audioModel.synth.start()
@@ -41,6 +46,6 @@ struct TestView: View {
 
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
-        TestView()
+        TestView().environmentObject(ViewModel())
     }
 }
